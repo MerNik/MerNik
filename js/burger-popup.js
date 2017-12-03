9998251d4eaafce6317274aa441cbb495141bab9
MerNik <31828231+MerNik@users.noreply.github.com>
2017-12-03 11:19:35 +0300

    fix

--- js/burger-popup.js
+++ js/burger-popup.js
@@ -1,54 +1,20 @@
-/*let popup = (function (options) {
+let acc = document.querySelector('.menu__list'),
+    list = acc.children
 
-    let consist = document.querySelector(options.consist);
-    let popup = document.querySelector(options.popup);
-    //   let body = document.querySelector('body');
-
-    let _toggleMenu = function (e) {
-        consist.classList.toggle('burgers__consist_active');
-        popup.classList.toggle('burgers__consist-popup_open');
-        // body.classList.toggle('body-active-menu');
-    }
-
-    let addListeners = function () {
-        consist.addEventListener('click', _toggleMenu);
-    }
-
-    return {
-        init: addListeners
-    };
-})({
-    consist: '#consist',
-    popup: '#consist-popup'
-});
-
-popup.init();
-// прокрутка при нажатии на стрелки вправо или влево
-$('.arrow').click(function(e){
-    e.preventDefault();
-    var $this = $(this),
-        container = $('.burgers-section__slider'),
-        items = $('.slider__item', container),
-        activeItem = items.filter('.active'),
-        existedItem, edgeItem, reqItem;
-
-    if ($this.hasClass('arrow-right')) { //вперед
-        existedItem = activeItem.next();
-        edgeItem = items.first();
-    }
-    if ($this.hasClass('arrow-left')) { //назад
-        existedItem = activeItem.prev();
-        edgeItem = items.last();
-    }
-
-    reqItem = existedItem.length ? existedItem.index() : edgeItem.index();
-
-    moveSlide(container, reqItem);
-});
-};
-slider();*/
-
-var btn = document.getElementById("my1");
-btn.onclick = function(){
-    $("#my1").next(".modal").css("display","block");
+acc.addEventListener('click', event => {
+    let target = event.target
+    if (!target.classList.contains('menu__title')) {
+    return
 }
+event.preventDefault()
+target = target.parentNode
+if (target.classList.contains('menu__item_active')) {
+    target.classList.remove('menu__item_active')
+    return
+} else {
+    [...list].forEach(menu__item => {
+        menu__item.classList.remove('menu__item_active')
+    target.classList.add('menu__item_active')
+})
+}
+})
